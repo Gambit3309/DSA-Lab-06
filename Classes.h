3810fc919a85c133af7f9694b9ae67a225bf8c7c
Gambit <162299609+Gambit3309@users.noreply.github.com>
2025-12-12 15:27:23 +0500

    Add files via upload

--- Classes.h
+++ Classes.h
@@ -20,7 +20,7 @@ private:
     string name, gender, cnic, address, cellNo;
     int age;
     Date dob;          // composition: Person "has-a" Date
-protected:
+public:
     void setName(string);
     void setGender(string);
     void setCnic(string);
@@ -36,7 +36,6 @@ protected:
     string getCellNo()const;
     int getAge()const;
     Date getDob()const;
-public:
     Person(string = "", string = "", string = "", string = "", string = "", int = 0, Date = Date());
     void inputPerson();
     void displayPerson() const;
@@ -50,7 +49,7 @@ private:
     Date crimeDate;
     Date punishmentDate;
     string crimeDescription[10] = {"Murder", "Assault", "Robbery", "Drugs" , "Theft", "Burglary", "Arson", "Fraud", "Embezzlement", "Driving Under the Influence (DUI/DWI)"};
-protected:
+public:
     void setCrimeType(int);
     void setComplainerName(string);
     void setInvestiOffi(string);
@@ -66,7 +65,6 @@ protected:
     Date getPuniDate()const;
     string getCrimeDescription(int)const;
 
-public:
     Criminal(string = "", string = "", string ="", string = "", string = "", int = 0, Date = Date(), int = 0 , string= "", string = "", int = 0, Date = Date(), Date = Date());
     Criminal(const Criminal &c);
     void inputCriminal();
@@ -81,14 +79,13 @@ private:
     Criminal criminals[Criminal_List_Size];
     int count;
 
-protected:
+public:
     void setcriminal(Criminal, int);
     void increaseCount();
 
     Criminal getCriminal(int)const;
     int getcount()const;
 
-public:
     PoliceRecordSystem();   //done
     void addCriminal(); //done
     void displayAll()const; //done
